b490bda3ff2af01facbe23b5e0e52b494ad5c2a6
djumanov <djumanovdev@gmail.com>
2025-11-12 17:08:01 +0500

    one to one

--- api/models.py
+++ api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=128, unique=True)
+    password = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.username
+
+
+class UserSettings(models.Model):
+    LAN_CHOICES = [
+        ['en', 'English'],
+        ['uz', 'Uzbek'],
+        ['ru', 'Russian'],
+    ]
+    MODE_CHOICES = [
+        ['dark', 'Dark'],
+        ['light', 'Light'],
+    ]
+    lang = models.CharField(max_length=10, choices=LAN_CHOICES, default='en')
+    mode = models.CharField(max_length=10, choices=MODE_CHOICES, default='light')
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE, related_name='settings')
+
+    def __str__(self):
+        return self.user.username
+    
